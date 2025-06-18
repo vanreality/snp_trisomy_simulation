55bb58e53095bed485de194d91a9d85f7eca74d9
vanreality <1413242514@qq.com>
2025-06-18 16:51:32 +0800

    Enhance simulation parameters in job submission scripts
    - Added configurable parameters for simulated fetal fraction range and number of repeats in `run_lr_calculator.sh`.
    - Updated SLURM job submission to include new parameters for fetal fraction estimation in `run_lr_calculator.slurm`.
    - Improved flexibility for simulation configurations based on user input.

--- run_lr_calculator.sh
+++ run_lr_calculator.sh
@@ -17,6 +17,11 @@ labels=("disomy" "trisomy")
 # Get mode: "cfDNA", "cfDNA+WBC", or "cfDNA+model"
 mode=$1
 
+simulated_ff_min=0.01
+simulated_ff_max=0.10
+simulated_ff_step=0.01
+num_repeats=30
+
 # Loop through each simulation directory
 for snp_set in "${snp_sets[@]}"; do
     input_dir="${root_dir}/simulated_sequencing_data_${snp_set}"
@@ -26,7 +31,16 @@ for snp_set in "${snp_sets[@]}"; do
     for label in "${labels[@]}"; do
         for depth in $(seq 80 20 200); do
             # Submit SLURM job
-            sbatch run_lr_calculator.slurm "${input_dir}" "${label}" "${depth}" "${output_dir}" "${mode}"
+            sbatch run_lr_calculator.slurm \
+            "${input_dir}" \
+            "${label}" \
+            "${depth}" \
+            "${output_dir}" \
+            "${mode}" \
+            "${simulated_ff_min}" \
+            "${simulated_ff_max}" \
+            "${simulated_ff_step}" \
+            "${num_repeats}"
         done
     done
 done
